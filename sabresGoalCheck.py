82b55f3d338fc4e50f78d5c9a1aad8ddb45efe24
Untuxed <lcm7498@gmail.com>
2024-01-28 14:17:00 -0500

    Update to code structure and comments

--- sabresGoalCheck.py
+++ sabresGoalCheck.py
@@ -378,7 +378,7 @@ def main(page: ft.Page):
     # Main code loop
     baseAPIURL = 'https://api-web.nhle.com/'
     while True:
-        FullDateToday = datetime.datetime.now() - datetime.timedelta(days=1)
+        FullDateToday = datetime.datetime.now()
         today_date = FullDateToday.strftime("%Y-%m-%d")
         next_date = FullDateToday + datetime.timedelta(days=1)
         next_date = next_date.replace(hour=4, minute=0, second=0, microsecond=0)
